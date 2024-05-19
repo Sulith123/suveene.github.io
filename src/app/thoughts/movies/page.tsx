38b5408a507abc5e633ea1d2a940ed495fe6735f
SuveenE <suveen.te1@gmail.com>
2024-05-19 18:59:35 +0800

    Update movies blog

--- src/app/thoughts/movies/page.tsx
+++ src/app/thoughts/movies/page.tsx
@@ -1,30 +1,42 @@
-import MovieCard from "./movie-card";
-
-const Movies = ()=> {
+const Movies = () => {
   return (
     <main className="flex min-h-screen flex-col items-center p-6 lg:pb-12 lg:px-24 lg:pt-16 font-mono bg-white">
       <p className="font-semibold text-2xl mb-4">MY MOVIE RECCOMENDATIONS 📽️</p>
-      <p className="md:w-[60%] my-5">
-        I actually don&apos;t expect many people to visit this webpage. So if
-        you are here, I do not want dissapoint you with some famous good
-        movies. Here are some lesser known movies.
-      </p>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-4 items-center">
-      <MovieCard
-        name="12 Angry Men (1957)"
-        description="Amazing B&W movie. Felt like reading a book."
-      />
-      <MovieCard
-        name="Mystic River (2003)"
-        description="Great performance by Sean Penn."
-      />
-      <MovieCard
-        name="1917 (2019)"
-        description="The movie is shot in a way that it looks like a single continous shot."
-      />
+      <div className="md:w-[60%]">
+        <p className="my-5">
+          I don&apos;t expect many visitors here, but for those who do, I
+          don&apos;t want to disappoint with the usual famous movies. Instead,
+          here are some <span className="italic font-semibold">lesser-known gems</span> in no particular order.
+        </p>
+
+        <div>
+          <span className="font-bold mb-1">12 Angry Men </span>(1957) - Amazing
+          B&W movie. Felt like reading a book.
+        </div>
+        <div>
+          <span className="font-bold mb-1">Mystic River </span>(2003) - Great
+          performance by Sean Penn.
+        </div>
+        <div>
+          <span className="font-bold mb-1">1917 </span>(2019) - The war movie is
+          filmed to appear as if it was done in a single continuous shot.
+        </div>
+        <div>
+          <span className="font-bold mb-1">Bangalore Days </span>(2014) -
+          Arguably the best Malayalam movie.
+        </div>
+        <div>
+          <span className="font-bold mb-1">Ratsasan </span>(2018) - Super well
+          scripted. One of the best psychological thrillers I&apos;ve ever
+          watched.
+        </div>
+        <div>
+          <span className="font-bold mb-1">The Breakfast Club </span>(1985) - Great coming-of-age movie from the 80s.
+          Got to know about this classic from the movie Pitch Perfect.
+        </div>
       </div>
     </main>
   );
-}
+};
 
 export default Movies;
