fba735555e44971c574b63ae270bb8ac949b2852
SuveenE <suveen.te1@gmail.com>
2024-05-27 20:35:19 +0800

    Update thoughts

--- src/app/thoughts/life/page.tsx
+++ src/app/thoughts/life/page.tsx
@@ -1,20 +1,20 @@
 const Life = () => {
     return (
       <main className="flex min-h-screen flex-col items-center p-6 lg:pb-12 lg:px-24 lg:pt-16 font-mono bg-white">
-        <p className="font-semibold text-2xl mb-4">MY LIFE IN BULLET POINTS</p>
+        <p className="font-semibold text-2xl mb-4"><span className="text-3xl mr-2">🌍</span> MY LIFE IN BULLET POINTS</p>
         <div className="md:w-[60%]">
           <div className="my-5">
            <p>1. Born in Sri Lanka.</p>
            <p>2. Got into top 200 chess players in the country.</p>
            <p>3. Won a TV quiz at 15.</p>
            <p>4. Became 12th in Sri Lanka in ALs (High school exam).</p>
-           <p>5. Interned at the biggest telemedicine startup in SL as a high school grad.</p>
+           <p>5. Interned at oDoc, the biggest telemedicine startup in SL, as a high school grad.</p>
            <p>6. Published Beratiles, an Android app that won some awards and 750+ downloads.</p>
            <p>7. Moved to Singapore. Got into National University of Singapore for bachelors in Computer Engineering.</p>
-           <p>8. Started working part time as a dev at AristotleHQ - an AI startup.</p>
-           <p>9. Worked in Vietnam for 3 months in a storage startup.</p>
+           <p>8. Started working part time as a software engineer at AristotleHQ - an AI startup.</p>
+           <p>9. Spent 3 months in Vietnam while working at Mystorage, a storage startup.</p>
            <p>10. Published a research paper as a co-author.</p>
-           <p>11. Got SGD 10,000 grant for a project  from the uni.</p>
+           <p>11. Got SGD 10,000 grant for a project  from my university(NUS).</p>
            <p>12. Got into a Silicon Valley internship program and will be spending the next year there.</p>
            <p>13. Started working as a research intern at Augmented Human Lab at NUS.</p>
           </div>
