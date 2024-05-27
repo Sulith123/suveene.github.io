b58d94e733099790b653e73410d6f601fb069a48
SuveenE <suveen.te1@gmail.com>
2024-05-27 20:38:08 +0800

    Fix typo

--- src/app/thoughts/page.tsx
+++ src/app/thoughts/page.tsx
@@ -6,7 +6,7 @@ export default function Thoughts() {
     const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-start p-6 lg:pb-12 lg:px-24 lg:pt-16 font-mono bg-white">
-      <p className="mx-auto text-center text-lg mb-5">Some things I wanted to wrote about</p>
+      <p className="mx-auto text-center text-lg mb-5">Some things I wanted to write about</p>
       <div className="md:w-[60%] mx-auto">
       <p  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/movies")}}>🍿 My Movie Recommedations</p>
       <p  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/life")}}>🌍 My life in bullet points</p>
