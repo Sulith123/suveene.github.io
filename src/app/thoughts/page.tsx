5cd4acb38f4a174d63734c74bc3d734a39e47eff
SuveenE <suveen.te1@gmail.com>
2024-05-27 22:29:56 +0800

    update thoughts home

--- src/app/thoughts/page.tsx
+++ src/app/thoughts/page.tsx
@@ -8,9 +8,9 @@ export default function Thoughts() {
     <main className="flex min-h-screen flex-col items-start p-6 lg:pb-12 lg:px-24 lg:pt-16 font-mono bg-white">
       <p className="mx-auto text-center text-lg mb-5">Some things I wanted to write about</p>
       <div className="md:w-[60%] mx-auto">
-      <p  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/movies")}}>🍿 My Movie Recommedations</p>
-      <p  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/life")}}>🌍 My life in bullet points</p>
-      <p  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/vietnam")}}>💥 My 3 months in Vietnam</p>
+      <p >🍿 <span  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/movies")}}>My Movie Recommedations</span></p>
+      <p >🌍 <span  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/life")}}>My life in bullet points</span></p>
+      <p >💥 <span  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/vietnam")}}>3 months in Vietnam</span></p>
       </div>
     </main>
   );
