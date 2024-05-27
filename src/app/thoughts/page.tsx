05db899767a1ccbc65797ed3e67b71d72d480e8d
SuveenE <suveen.te1@gmail.com>
2024-05-27 20:19:10 +0800

    Update thoughts page

--- src/app/thoughts/page.tsx
+++ src/app/thoughts/page.tsx
@@ -6,9 +6,12 @@ export default function Thoughts() {
     const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-start p-6 lg:pb-12 lg:px-24 lg:pt-16 font-mono bg-white">
-      <p  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/movies")}}>My Movie Recommedations</p>
-      <p  className="underline cursor-pointer" >My 3 months in Vietnam (WIP)</p>
+      <p className="mx-auto text-center text-lg mb-5">Some things I wanted to wrote about</p>
+      <div className="md:w-[60%] mx-auto">
+      <p  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/movies")}}>📽️ My Movie Recommedations</p>
+      <p  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/vietnam")}}>💥 My 3 months in Vietnam</p>
       <p  className="underline cursor-pointer" ></p>
+      </div>
     </main>
   );
 }
