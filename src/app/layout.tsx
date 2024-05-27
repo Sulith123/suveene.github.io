2d5722c83610ae2f79b8632b5e6d2a48b90d820b
SuveenE <suveen.te1@gmail.com>
2024-05-27 20:04:41 +0800

    Add thoughts section to home

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,12 +21,12 @@ export default function RootLayout({
     <html lang="en" data-theme='light'>
       <head><link rel="shortcut icon" href="/dino.svg" /></head>
       <body className={`${inter.className} bg-white`}>
-        <div className="flex flex-row mx-auto gap-3 md:gap-6 font-mono text-sm md:text-base font-bold p-3 w-full mt-5 sticky top-0 z-20 bg-white">
+        <div className="flex flex-row mx-auto gap-3 md:gap-6 font-mono text-xs md:text-base font-bold p-3 w-full mt-5 sticky top-0 z-20 bg-white">
           <a href="/#about-me" className="cursor-pointer ml-auto hover:scale-110 text-center">About Me</a>
           <a href="/#experience" className="cursor-pointer hover:scale-110 text-center">Experience</a>
           <a href="/#projects" className="cursor-pointer hover:scale-110 text-center">Projects</a>
-          <a href="/#other-stuff" className="cursor-pointer mr-auto hover:scale-110 text-center">Other Stuff</a>
-          {/* <a href="/#other-stuff" className="cursor-pointer mr-auto hover:scale-110 text-center">Thoughts</a> */}
+          <a href="/#other-stuff" className="cursor-pointer hover:scale-110 text-center">Other Stuff</a>
+          <a href="/thoughts" className="cursor-pointer mr-auto hover:scale-110 text-center">Thoughts</a>
         </div>
         {children}
       </body>
