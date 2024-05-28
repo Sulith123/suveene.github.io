4321778546aa642f6deb65009b567f764dc0c99a
SuveenE <suveen.te1@gmail.com>
2024-05-29 01:00:16 +0800

    Add AI see project

--- src/app/components/home/projects-section/index.tsx
+++ src/app/components/home/projects-section/index.tsx
@@ -10,6 +10,14 @@ const ProjectsSection = () => {
         </p>
       </div>
       <div className="grid lg:w-[70%] grid-cols-1 lg:grid-cols-1 lg:mx-auto lg:gap-5 gap-3 items-center">
+        
+        <ProjectCard
+          name="AI See"
+          image="/AIsee.jpg"
+          description="AI See is an affordable wearable assistive device that helps people with visual impairment ‘see’ objects around them with the help of artificial intelligence (AI)."
+          period="2024"
+          url="https://ahlab.org/project/aisee/"
+        />
         <ProjectCard
           name="Bera Tiles"
           image="/beratiles.jpg"
@@ -17,14 +25,7 @@ const ProjectsSection = () => {
           period="2022"
           url="https://play.google.com/store/apps/details?id=com.suveen.bera_tiles_1"
         />
-        {/* <ProjectCard
-          name="Bera Tiles"
-          image="/beratiles.jpg"
-          description="Bera Tiles is a mobile app that gamifies the essence of Sri Lankan traditional drums. The app features digital renditions of over 6 Sri Lankan traditional drums, coupled with a reflex-testing game akin to Piano Tiles."
-          period="2022"
-          url="https://play.google.com/store/apps/details?id=com.suveen.bera_tiles_1"
-        />
-        <ProjectCard
+        {/*<ProjectCard
           name="Bera Tiles"
           image="/beratiles.jpg"
           description="Bera Tiles is a mobile app that gamifies the essence of Sri Lankan traditional drums. The app features digital renditions of over 6 Sri Lankan traditional drums, coupled with a reflex-testing game akin to Piano Tiles."
