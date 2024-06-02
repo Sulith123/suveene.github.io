556f0d411b532729876f583a221e37b66cfd8d2b
SuveenE <suveen.te1@gmail.com>
2024-06-02 12:24:48 +0800

    Add walk page to thoughts

--- src/app/thoughts/page.tsx
+++ src/app/thoughts/page.tsx
@@ -6,10 +6,11 @@ export default function Thoughts() {
     const router = useRouter();
   return (
     <main className="flex min-h-screen flex-col items-start p-6 lg:pb-12 lg:px-24 lg:pt-16 font-mono bg-white">
-      <p className="mx-auto text-center text-lg mb-5">Some things I wanted to write about</p>
+      <p className="mx-auto text-center text-lg mb-5">Almost like a blog.</p>
       <div className="md:w-[60%] mx-auto">
+      <p >🚶 <span  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/the-walk")}}>How we walked across Singapore</span></p>
       <p >🍿 <span  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/movies")}}>My Movie Recommedations</span></p>
-      <p >🌍 <span  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/life")}}>My life in bullet points</span></p>
+      {/* <p >🌍 <span  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/life")}}>My life in bullet points</span></p> */}
       <p >💥 <span  className="underline cursor-pointer" onClick={()=> {router.push("/thoughts/vietnam")}}>3 months in Vietnam</span></p>
       </div>
     </main>
