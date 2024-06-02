64a790d4a11d3614bb97833fbb8b4e7231f28779
SuveenE <suveen.te1@gmail.com>
2024-06-02 13:15:26 +0800

    Add why section to the-walk

--- src/app/thoughts/the-walk/page.tsx
+++ src/app/thoughts/the-walk/page.tsx
@@ -4,10 +4,10 @@ const Walk = () => {
   return (
     <main className="flex min-h-screen flex-col md:w-[66%] mx-auto p-6 lg:pb-12 lg:px-24 lg:pt-16 font-mono bg-white">
       <p className="font-semibold text-2xl mb-4 mx-auto">
-        <span className="text-3xl mr-2 " >🚶</span>HOW WE WALKED ACROSS SINGAPORE?
+        <span className="text-3xl mr-2 ">🚶</span>HOW WE WALKED ACROSS
+        SINGAPORE?
       </p>
-      <p className="mb-4 text-lg font-bold">TL;DR
-</p>
+      <p className="mb-4 text-lg font-bold">TL;DR</p>
       <div className=" flex flex-col gap-2 md:flex-row">
         <div className="flex flex-col md:w-[60%]">
           <p className="mb-4">
@@ -31,11 +31,15 @@ const Walk = () => {
           </p>
           <p>
             <span className="font-semibold">Route:</span> Tuas Link MRT ={">"} -
-            Spring Leaf Prata, West Coast (Dinner) ={">"}Commenwealth ={">"} Clarke Quay ={">"} East Coast Park ={">"} Changi City Point
+            Spring Leaf Prata, West Coast (Dinner) ={">"}Commenwealth ={">"}{" "}
+            Clarke Quay ={">"} East Coast Park ={">"} Changi City Point
           </p>
           <p className="mt-4">
             <span className="font-semibold ">What we gained:</span> Sore legs,
-            blistered feet, and <span className="font-bold ">THE ULTIMATE BRAGGING RIGHTS FOR WALKING ACROSS AN ENTIRE COUNTRY.</span>
+            blistered feet, and{" "}
+            <span className="font-bold ">
+              THE ULTIMATE BRAGGING RIGHTS FOR WALKING ACROSS AN ENTIRE COUNTRY.
+            </span>
           </p>
         </div>
         <div>
@@ -49,7 +53,32 @@ const Walk = () => {
           />
         </div>
       </div>
-      
+      <div>
+        <p className="my-4 text-lg font-bold">Why did we even do this?</p>
+        <p className="my-1">
+          About two months ago, a few of us stumbled upon an Instagram reel
+          where a group of friends walked 64km across the country in 15 hours.
+          Inspired, we decided to give it a try. 
+        </p>
+        <p className="my-1">
+        We pitched the idea to others, our group grew to
+          18 adventurous souls ready to embark on this not-so-common-Friday-night-activity.
+        </p>
+        
+        <p className="my-1">
+          But why did we do this? We still don’t have a clear answer. Maybe, like
+          Forrest Gump felt like running, we just felt like walking a long
+          distance.
+        </p>
+        <Image
+            className="mx-auto md:mr-auto rounded-md my-4"
+            src="/thoughts/the-walk/why.gif"
+            alt="company logo"
+            width={320}
+            height={320}
+            priority
+          />
+      </div>
     </main>
   );
 };
